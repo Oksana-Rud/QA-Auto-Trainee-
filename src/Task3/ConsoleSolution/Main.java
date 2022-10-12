f87324b91f11b87570c9f51572969512e190c1b5
Oksana Rud <oksana.rud123@gmail.com>
2022-10-12 19:17:05 +0200

    add description to input and output

--- src/Task3/ConsoleSolution/Main.java
+++ src/Task3/ConsoleSolution/Main.java
@@ -1,11 +1,13 @@
 package Task3.ConsoleSolution;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[]{34, 56, 22, 27, 78, 234, 5, 101, 66, 9};
-        System.out.println(outputArray(array));
+        int[] array = new int[]{34, 56, 22, 27, 78, 234, 5, 101, 66, 9, 101, 13, 234, 34};
+        System.out.println("Дан массив целых чисел: " + Arrays.toString(array));
+        System.out.println("Массив чисел кратных 3: " + outputArray(array));
     }
 
     public static ArrayList<Integer> outputArray(int[] array) {
